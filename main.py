f620e277d7e32b7fd219002cff09cf1be5aeae53
ilya@hackerdom.ru <m6uDjk35cP2h>
2021-07-27 15:56:56 +0300

    Add Google auth.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, abort
 from flask import make_response, session, request
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from sqlalchemy.sql.expression import func
 from sqlalchemy.exc import SQLAlchemyError
+from google_auth_oauthlib.flow import Flow
+import google.auth.transport.requests
+from google.oauth2 import id_token
+from pip._vendor import cachecontrol
 
 import requests
 import json
 import sys, os
+import pathlib
 
 
 from models import db, init_db, Users, ThesesThemes, Level
@@ -28,23 +33,80 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+# Google auth (https://github.com/code-specialist/flask_google_login/blob/main/app.py)
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "593053078492-i6hf335m9hm0vtj23df62q09j07esbhu.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/google_callback"
+)
+
 @login_manager.user_loader
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/google_login")
+def google_login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
 
-@app.route('/register-basic.html')
-def register_basic():
-    return render_template("register-basic.html")
+@app.route('/google_callback')
+def google_callback():
 
+    flow.fetch_token(authorization_response=request.url)
 
-@app.route('/password-recovery.html')
-def password_recovery():
-    return render_template("password-recovery.html")
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
 
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    print (id_info)
+
+    avatar_uri = os.urandom(16).hex()
+    avatar_uri = avatar_uri + ".jpg"
+
+    if 'picture' in id_info:
+        r = requests.get(id_info.get('picture'), allow_redirects=True)
+        print(r.status_code)
+        open('static/assets/avatars/' + avatar_uri, 'wb').write(r.content)
+
+    user = Users.query.filter_by(google_id=id_info.get('email')).first()
+
+    # New user?
+    if user is None:
+        # Yes
+        try:
+            new_user = Users(last_name=id_info.get('family_name'),
+                             first_name=id_info.get('given_name'),
+                             avatar_uri=avatar_uri,
+                             google_id=id_info.get('email'))
+            db.session.add(new_user)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            error = str(e.__dict__['orig'])
+            print(error)
+            print("Ошибка при добавлении пользователя в БД")
+            return redirect(url_for('index'))
+
+        user = Users.query.filter_by(google_id=id_info.get('email')).first()
+
+    login_user(user, remember=True)
+    return redirect(url_for('lk'))
 
 # https://vk.com/dev/authcode_flow_user
 @app.route("/vk_callback")
@@ -68,8 +130,6 @@ def vk_callback():
     # Get user name
     response = requests.get('https://api.vk.com/method/users.get?user_ids=' + str(vk_id) + '&fields=bdate,photo_100&access_token=' + str(access_token) + '&v=5.130')
     vk_user = json.loads(response.text)
-
-    print (vk_user)
 
     avatar_uri = os.urandom(16).hex()
     avatar_uri = avatar_uri + ".jpg"
@@ -103,6 +163,18 @@ def vk_callback():
     login_user(user, remember=True)
     return redirect(url_for('lk'))
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register-basic.html')
+def register_basic():
+    return render_template("register-basic.html")
+
+
+@app.route('/password-recovery.html')
+def password_recovery():
+    return render_template("password-recovery.html")
 
 @app.route('/lk.html')
 @login_required
