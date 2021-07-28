bf4222959000c22176b8bbdfdd04e8ed54d63e71
«Yelena <yelenazvyagina@gmail.com>
2021-07-29 02:04:29 +0700

    email authorization

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
-import sys, os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, abort
+from flask import make_response, session, request, flash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from sqlalchemy.sql.expression import func
+from sqlalchemy.exc import SQLAlchemyError
+from google_auth_oauthlib.flow import Flow
+import google.auth.transport.requests
+from google.oauth2 import id_token
+from pip._vendor import cachecontrol
+from werkzeug.security import generate_password_hash, check_password_hash
+
+import requests
+import json
+import sys, os
+import pathlib
 
 from models import db, init_db, Users, ThesesThemes, Level
+
 app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
 
 # Flask configs
@@ -18,21 +31,180 @@ app.config['SECRET_KEY'] = os.urandom(16).hex()
 db.app = app
 db.init_app(app)
 
-@app.route('/')
-def start():
-    return render_template("index.html")
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Google auth (https://github.com/code-specialist/flask_google_login/blob/main/app.py)
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "593053078492-i6hf335m9hm0vtj23df62q09j07esbhu.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email",
+            "openid"],
+    redirect_uri="http://127.0.0.1:5000/google_callback"
+)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
-@app.route('/lk.html')
-def lk():
-    user = Users.query.filter_by(id=1).first()
 
-    return render_template('lk.html', user=user)
+@app.route("/google_login")
+def google_login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
 
-@app.route('/register-basic.html')
+
+@app.route('/google_callback')
+def google_callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    user = Users.query.filter_by(google_id=id_info.get('email')).first()
+
+    # New user?
+    if user is None:
+        # Yes
+        try:
+            avatar_uri = os.urandom(16).hex()
+            avatar_uri = avatar_uri + ".jpg"
+
+            if 'picture' in id_info:
+                r = requests.get(id_info.get('picture'), allow_redirects=True)
+                open('static/assets/avatars/' + avatar_uri, 'wb').write(r.content)
+
+            new_user = Users(last_name=id_info.get('family_name'),
+                             first_name=id_info.get('given_name'),
+                             avatar_uri=avatar_uri,
+                             google_id=id_info.get('email'))
+            db.session.add(new_user)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            error = str(e.__dict__['orig'])
+            print(error)
+            print("Ошибка при добавлении пользователя в БД")
+            return redirect(url_for('index'))
+
+        user = Users.query.filter_by(google_id=id_info.get('email')).first()
+
+    login_user(user, remember=True)
+    return redirect(url_for('lk'))
+
+
+# https://vk.com/dev/authcode_flow_user
+@app.route("/vk_callback")
+def vk_callback():
+    user_code = request.args.get('code')
+
+    if not user_code:
+        return redirect(url_for('index'))
+
+    # Get access token
+    response = requests.get(
+        'https://oauth.vk.com/access_token?client_id=7912054&client_secret=rIE4Ef9876ktwXHFrgyU&redirect_uri=http://127.0.0.1:5000/vk_callback&code=' + user_code)
+    access_token_json = json.loads(response.text)
+
+    if "error" in access_token_json:
+        return redirect(url_for('index'))
+
+    vk_id = access_token_json['user_id']
+    access_token = access_token_json['access_token']
+
+    # Get user name
+    response = requests.get(
+        'https://api.vk.com/method/users.get?user_ids=' + str(vk_id) + '&fields=photo_100&access_token=' + str(
+            access_token) + '&v=5.130')
+    vk_user = json.loads(response.text)
+
+    user = Users.query.filter_by(vk_id=vk_id).first()
+
+    # New user?
+    if user is None:
+        # Yes
+        try:
+            avatar_uri = os.urandom(16).hex()
+            avatar_uri = avatar_uri + ".jpg"
+
+            if 'photo_100' in vk_user['response'][0]:
+                r = requests.get(vk_user['response'][0]['photo_100'], allow_redirects=True)
+                open('static/assets/avatars/' + avatar_uri, 'wb').write(r.content)
+
+            new_user = Users(last_name=vk_user['response'][0]['last_name'],
+                             first_name=vk_user['response'][0]['first_name'],
+                             avatar_uri=avatar_uri,
+                             vk_id=vk_id, vkaccesstoken=access_token)
+            db.session.add(new_user)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            error = str(e.__dict__['orig'])
+            print(error)
+            print("Ошибка при добавлении пользователя в БД")
+            return redirect(url_for('index'))
+
+        user = Users.query.filter_by(vk_id=vk_id).first()
+
+    login_user(user, remember=True)
+    return redirect(url_for('lk'))
+
+
+# login
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = Users.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password_hash, password):
+                login_user(user, remember=True)
+                return redirect(url_for('lk'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    return render_template('index.html', user=current_user)
+
+
+@app.route('/register-basic.html', methods=['GET', 'POST'])
 def register_basic():
-    return render_template("register-basic.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = Users.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(password) < 4:
+            flash('Password must be at least 4 characters.', category='error')
+        else:
+            new_user = Users(email=email, password_hash=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            return redirect(url_for('lk'))
+    return render_template("register-basic.html", user=current_user)
 
 
 @app.route('/password-recovery.html')
@@ -40,9 +212,15 @@ def password_recovery():
     return render_template("password-recovery.html")
 
 
-if __name__ == '__main__':
+@app.route('/lk.html')
+@login_required
+def lk():
+    user = Users.query.filter_by(id=current_user.id).first()
+    return render_template('lk.html', user=user)
 
+
+if __name__ == '__main__':
     if (len(sys.argv) > 1) and (sys.argv[1] == "init"):
         init_db()
     else:
-        app.run(port=5001)
+        app.run(port=5000)
--- models.py
+++ models.py
@@ -1,16 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from os import urandom
 
 db = SQLAlchemy()
 
-class Users(db.Model):
+
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     email = db.Column(db.String(255), unique=True, nullable=True)
     password_hash = db.Column(db.String(255), unique=False, nullable=True)
 
-    first_name = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(255), nullable=True)
     middle_name = db.Column(db.String(255), nullable=True)
     last_name = db.Column(db.String(255), nullable=True)
 
@@ -19,6 +21,7 @@ class Users(db.Model):
     contacts = db.Column(db.String(512), nullable=True)
 
     vk_id = db.Column(db.String(255), nullable=True)
+    vkaccesstoken = db.Column(db.String(512), nullable=True)
     fb_id = db.Column(db.String(255), nullable=True)
     google_id = db.Column(db.String(255), nullable=True)
 
@@ -33,11 +36,13 @@ class Users(db.Model):
     def __repr__(self):
         return f"{self.last_name}, {self.first_name}, {self.middle_name}"
 
+
 class Department(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    title= db.Column(db.String(255), default='', nullable=False)
+    title = db.Column(db.String(255), default='', nullable=False)
     logo = db.Column(db.String(255), default='empty.png', nullable=False)
+
 
 class ThesesThemes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -46,36 +51,38 @@ class ThesesThemes(db.Model):
 
     title_ru = db.Column(db.String(255), default='', nullable=False)
     description = db.Column(db.String(1024), default='', nullable=True)
-    level_id = db.Column(db.Integer, db.ForeignKey('level.id'), default=1, nullable=False)
+    level_id = db.Column(db.Integer, db.ForeignKey('level.id'), default=1, nullable=True)
 
     supervisor_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
     advisor_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
 
+    publish_year = db.Column(db.Integer, default=2021, nullable=False)
+
     requirements = db.Column(db.String(512), nullable=True)
 
-class Level(db.Model):
 
+class Level(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), default='', nullable=False)
 
-def init_db():
 
+def init_db():
     users = [
-        {'email' : 'ilya@hackerdom.ru', 'last_name' : 'Зеленчук', 'first_name' : 'Илья',
-         'avatar_uri' : 'zelenchuk.jpg'}
-        ]
+        {'email': 'ilya@hackerdom.ru', 'last_name': 'Зеленчук', 'first_name': 'Илья',
+         'avatar_uri': 'zelenchuk.jpg'}
+    ]
 
     # Init DB
-    db.session.commit() # https://stackoverflow.com/questions/24289808/drop-all-freezes-in-flask-with-sqlalchemy
+    db.session.commit()  # https://stackoverflow.com/questions/24289808/drop-all-freezes-in-flask-with-sqlalchemy
     db.drop_all()
     db.create_all()
 
     # Create users
-    print ("Create users")
+    print("Create users")
     for user in users:
-        u = Users(email=user['email'], password_hash = generate_password_hash(urandom(16).hex()), first_name = user['first_name'], last_name = user['last_name'],
-                 avatar_uri = user['avatar_uri'])
+        u = Users(email=user['email'], password_hash=generate_password_hash(urandom(16).hex()),
+                  first_name=user['first_name'], last_name=user['last_name'],
+                  avatar_uri=user['avatar_uri'])
 
         db.session.add(u)
         db.session.commit()
-
